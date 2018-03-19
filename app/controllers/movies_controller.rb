b1b1dcbd5a16af22beb700893d6d9a0bddb80454
adel <adel.almaleh@resolutions.com.my>
2018-03-19 20:05:32 +0800

    added header and footer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -71,4 +71,9 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).permit(:title, :description, :director, :stars, :release)
     end
+    
+    def user_logged_in?
+      return true if user_signed_in?
+      redirect_to new_user_session_path, data: {confirm: "You sure" }
+    end
 end
