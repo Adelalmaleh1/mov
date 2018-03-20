0670a343d2e69abf631caa7ee42630dc630ee286
adel <adel.almaleh@resolutions.com.my>
2018-03-20 09:28:15 +0800

    added username to devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users do 
-    
-  end
-  resources :movies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'  
+    devise_for :users
+
+devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+resources :movies
+    root 'welcome#index'  
 end
