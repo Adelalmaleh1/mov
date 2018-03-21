490e0f4ab800bf2fd9fbe6d795d96e8c929eddbd
adel <adel.almaleh@resolutions.com.my>
2018-03-21 22:06:11 +0800

    Added user's show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+# match '/users',   to: 'users#index',   via: 'get'
+# match '/users/:id',   to: 'users#show',   via: 'get'
+get 'user/:id' => 'users#show', as: :user
+  get 'users' => 'users#index', as: :user_index
+
+
+
   resources :categories
     devise_for :users
 
