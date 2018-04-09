dfcfee9737e4ff4d54c064c6ef7dc1be07615e19
adel <adel.almaleh@resolutions.com.my>
2018-04-09 16:37:02 +0800

    working on deploy the app to aws.

--- Capfile
+++ Capfile
@@ -1,24 +1,15 @@
-# Load DSL and set up stages
+ #Load DSL and set up stages
 require "capistrano/setup"
 
 # Include default deployment tasks
 require "capistrano/deploy"
-require "capistrano/bundler"
-require "capistrano/rails/assets"
-require "capistrano/rails/migrations"
-# Load the SCM plugin appropriate to your project:
-#
-# require "capistrano/scm/hg"
-# install_plugin Capistrano::SCM::Hg
-# or
-# require "capistrano/scm/svn"
-# install_plugin Capistrano::SCM::Svn
-# or
-require "capistrano/scm/git"
-install_plugin Capistrano::SCM::Git
 
 # Include tasks from other gems included in your Gemfile
 #
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+# 
 # For documentation on these, see for example:
 #
 #   https://github.com/capistrano/rvm
@@ -28,13 +19,14 @@ install_plugin Capistrano::SCM::Git
 #   https://github.com/capistrano/rails
 #   https://github.com/capistrano/passenger
 #
-# require "capistrano/rvm"
-# require "capistrano/rbenv"
-# require "capistrano/chruby"
-# require "capistrano/bundler"
-# require "capistrano/rails/assets"
-# require "capistrano/rails/migrations"
-# require "capistrano/passenger"
+require 'capistrano/rvm'
+# require 'capistrano/rbenv'
+# require 'capistrano/chruby'
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
+require 'capistrano/puma'
+# require 'capistrano/passenger'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,18 +3,17 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server "13.229.103.84", user: "ubuntu", roles: %w{web app db}
-set :ssh_options, { forward_agent: true }
+
 
 # role-based syntax
 # ==================
 
 # Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
+# group is considered to be the first unless any  hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
@@ -50,13 +49,15 @@ set :ssh_options, { forward_agent: true }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
+# server 'example.com',
+#   user: 'user_name',
 #   roles: %w{web app},
 #   ssh_options: {
-#     user: "user_name", # overrides user setting above
+#     user: 'user_name', # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
-#     # password: "please use keys"
+#     # password: 'please use keys'
 #   }
+
+server '13.229.103.84', user: 'deploy', roles: %w{web app db}
